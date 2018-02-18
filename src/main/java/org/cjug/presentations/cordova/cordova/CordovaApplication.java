504f85a072106a1528efc278d58386dd4a0c0d9c
windust <freddy.guime@cjug.org>
2018-02-17 22:38:33 -0600

    Initial commit

---
+++ src/main/java/org/cjug/presentations/cordova/cordova/CordovaApplication.java
@@ -0,0 +1,12 @@
+package org.cjug.presentations.cordova.cordova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordovaApplication.class, args);
+	}
+}
